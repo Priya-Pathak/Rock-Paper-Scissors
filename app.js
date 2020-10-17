60c3876fb8e29b82e8be855d7468cefac5861658
Priya-Pathak <priyapathak24.07@gmail.com>
2020-10-17 15:40:45 +0530

    This is front-end of the rock paper scissors game.

---
+++ app.js
@@ -0,0 +1,6 @@
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+
